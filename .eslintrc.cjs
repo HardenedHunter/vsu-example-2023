f379d80effbdad6be8bce764cedcf1b193cf8082
HardenedHunter <specialforbattlenet@gmail.com>
2023-09-30 14:19:20 +0300

    Lecture 27.09

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,6 +15,8 @@ const config = {
     // Feel free to reconfigure them to your own preference.
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
+    "@typescript-eslint/no-floating-promises": "off",
+    "@next/next/no-img-element": "off",
 
     "@typescript-eslint/consistent-type-imports": [
       "warn",
--- src/pages/example.tsx
+++ src/pages/example.tsx
@@ -1,15 +1,13 @@
-type Employee = {
-  avatar: string;
-  name: string;
-  surname: string;
-  role: string;
-};
+import { useEffect, useState } from "react";
 
-const myEmployee: Employee = {
-  avatar: "https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg",
-  name: "Blanche",
-  surname: "Pearson",
-  role: "Sales Manager",
+import Spinner from "~/components/Spinner";
+
+type Employee = {
+  id: number;
+  image: string;
+  firstName: string;
+  lastName: string;
+  university: string;
 };
 
 type EmployeeCardProps = {
@@ -17,18 +15,53 @@ type EmployeeCardProps = {
 };
 
 const EmployeeCard = ({ employee }: EmployeeCardProps) => {
+  const fullName = `${employee.firstName} ${employee.lastName}`;
+
   return (
-    <div>
-      <p>{employee.name}</p>
-      <p>{employee.surname}</p>
+    <div className="flex flex-col items-center gap-4 rounded-2xl border-2 border-solid border-black px-8 py-6 text-center">
+      <img
+        className="h-24 w-24 rounded-full border-2 border-solid border-black"
+        src={employee.image}
+        alt="avatar"
+      />
+      <p className="font-semibold">{fullName}</p>
     </div>
   );
 };
 
+const fetchUsers = async () => {
+  const response = await fetch("https://dummyjson.com/users?limit=20");
+  const responseJson = (await response.json()) as { users: Employee[] };
+
+  return responseJson.users;
+};
+
 const Example = () => {
+  const [users, setUsers] = useState<Employee[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const getUsers = async () => {
+    setLoading(true);
+
+    const fetchedUsers = await fetchUsers();
+    setUsers(fetchedUsers);
+
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
   return (
-    <div className="flex h-screen items-center justify-center">
-      <EmployeeCard employee={myEmployee} />
+    <div className="flex h-screen flex-col gap-8 px-5 pt-12">
+      {loading && (
+        <div className="mx-auto mt-12">
+          <Spinner />
+        </div>
+      )}
+      {!loading &&
+        users.map((user) => <EmployeeCard employee={user} key={user.id} />)}
     </div>
   );
 };
